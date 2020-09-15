541f8afaac27682a8e22b2e09dd5fdfe49198c03
BBG\Oscar <tywuab@connect.ust.hk>
2020-09-16 04:10:36 +0800

    First Commit

---
+++ src/main/java/Lab1/Library.java
@@ -0,0 +1,8 @@
+public class Library {
+	public static void main(String args[]) {
+		System.out.println("Where there is a fire, commit and push.");
+	}
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
